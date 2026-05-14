b239d49cc7bacb6a7d298f1016882f5d1e66c134
Quoc Tri Do <quoctrido.jb@gmail.com>
2026-05-14 20:01:02 +0200

    update LoggingMiddleware to log status code

--- 2.handoff/middleware.go
+++ 2.handoff/middleware.go
@@ -1,14 +1,46 @@
 package main
 
 import (
+	"bufio"
 	"context"
+	"errors"
 	"fmt"
 	"log/slog"
+	"net"
 	"net/http"
 	"time"
 
 	"github.com/google/uuid"
 )
+
+type statusRecorder struct {
+	http.ResponseWriter
+	StatusCode int
+	Hijacked   bool
+}
+
+func (r *statusRecorder) WriteHeader(code int) {
+	r.StatusCode = code
+	r.ResponseWriter.WriteHeader(code)
+}
+
+// https://pkg.go.dev/net/http#FS, Crtl+F "ResponseWriter wrapper" -> Hijack && Flush
+// If in the future, this wrapper gets more complicated, recommend to use github.com/felixge/httpsnoop
+func (r *statusRecorder) Hijack() (net.Conn, *bufio.ReadWriter, error) {
+	h, ok := r.ResponseWriter.(http.Hijacker)
+	if !ok {
+		return nil, nil, errors.New("hijack not supported")
+	}
+	r.Hijacked = true
+	return h.Hijack()
+}
+
+func (r *statusRecorder) Flush() {
+	f, ok := r.ResponseWriter.(http.Flusher)
+	if ok {
+		f.Flush()
+	}
+}
 
 func TimeoutMiddleware(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -30,24 +62,44 @@ func CORSMiddleware(nextHandler http.Handler) http.Handler {
 func LoggingMiddleware(nextHandler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
-		panicked := true
+		wrappedWriter := &statusRecorder{ResponseWriter: w, StatusCode: 200}
 		defer func() {
-			if panicked {
+			duration := fmt.Sprintf("%dms", time.Since(start).Milliseconds())
+			requestID := r.Context().Value(requestIDKey).(string)
+			err := recover()
+			if err != nil {
 				slog.Error("request panicked",
 					"method", r.Method,
 					"path", r.URL.Path,
-					"duration", fmt.Sprintf("%dms", time.Since(start).Milliseconds()),
-					requestIDKey, r.Context().Value(requestIDKey).(string))
+					"err", err,
+					"duration", duration,
+					requestIDKey, requestID,
+				)
+				writeError(w, 500, ErrorMessageJSON{
+					ErrorCode: INTERNAL_SERVER_ERROR,
+					Message:   "Server panicked",
+					RequestID: requestID,
+				})
+				return
 			}
+			if wrappedWriter.Hijacked {
+				slog.Info("websocket connection",
+					"method", r.Method,
+					"path", r.URL.Path,
+					"duration", duration,
+					requestIDKey, requestID,
+				)
+				return
+			}
+			slog.Info("request completed",
+				"method", r.Method,
+				"path", r.URL.Path,
+				"status", wrappedWriter.StatusCode,
+				"duration", duration,
+				requestIDKey, requestID,
+			)
 		}()
-		nextHandler.ServeHTTP(w, r)
-		slog.Info("request completed",
-			"method", r.Method,
-			"path", r.URL.Path,
-			"duration", fmt.Sprintf("%dms", time.Since(start).Milliseconds()),
-			requestIDKey, r.Context().Value(requestIDKey),
-		)
-		panicked = false
+		nextHandler.ServeHTTP(wrappedWriter, r)
 	})
 }
 
